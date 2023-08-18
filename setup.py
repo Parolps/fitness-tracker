5264caf6d3b9f0304e7e2f0d678117555f65fe76
Parolps <samuelmassas604@gmail.com>
2023-08-18 19:20:24 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='tracking and classifying different exercises with a barbell',
+    author='Paulo Lopes',
+    license='',
+)
